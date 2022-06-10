91966eeb2f2b27d90c533b5a442a9c63d02bcd74
adameverson <adam.everson@brgaap.com>
2022-06-10 18:51:28 -0300

    Correção Contas Financeiras Duplicadas
    Foi verificado as Contas Financeiras Duplicadas remanejado os DocFinanceiros vinculados e deletado as Contas Financeiras Duplicadas. Também é alertado se a prestação possui Contas Financeiras com o mesmo IdentificacaoRegistro só que com ContaContabil diferente, sendo necessário consultar a OS qual a Conta Financeira que ainda existe, para remanejar os DocFinanceiros e deletar a Conta Financeira que não existe mais na OS.

--- csharpAPI/PG_DB_Connection.cs
+++ csharpAPI/PG_DB_Connection.cs
@@ -16,6 +16,7 @@ namespace csharpAPI
 */
 
 using System;
+using System.Collections.Generic;
 using Npgsql;
 
 namespace Driver
@@ -34,15 +35,21 @@ namespace Driver
 
         private static string Host = "localhost";
         private static string User = "postgres";
-        private static string DBname = "SESGO_27042022";
+        private static string DBname = "SESGO08062022_4";
         private static string Password = "postgres";
         private static string Port = "5432";
 
-        private static int qtdParaAnalise = 0;
+        //private static int qtdParaAnalise = 0;
+        private static int qtdRemovida;
+        private static bool flagIrregular;
+        private static List<int> deletadas;
 
         static void Main(string[] args)
         {
-            var txtPrestacoes = "81500, 81541, 81598, 81641, 81692, 81713, 81792";
+            string[] txtPrestacoes = { "81816", "81838" };
+            List<string> msgFinalQtdRemovida = new List<string>();
+            List<bool> msgFinalIrregular = new List<bool>();
+            List<List<int>> deletadasPorPrestacao = new List<List<int>>();
 
             /*
             Select("SELECT * FROM \"ContratosFornecedores\"");
@@ -51,16 +58,74 @@ namespace Driver
             Select("SELECT * FROM \"ContratosFornecedores\" WHERE \"IdentificacaoRegistro\" IS NULL");
             */
 
-            SelectCustomizado1($"SELECT COUNT(*) FROM \"ContratosFornecedores\" WHERE \"PrestacaoContaId\" IN ({txtPrestacoes}) AND \"IdentificacaoRegistro\" IS NOT NULL AND \"IdentificacaoRegistro\" != ''");
-            SelectCustomizado2($"SELECT \"PrestacaoContaId\", \"NumeroContrato\", \"IdentificacaoRegistro\" FROM \"ContratosFornecedores\" WHERE \"PrestacaoContaId\" IN ({txtPrestacoes}) AND \"IdentificacaoRegistro\" IS NOT NULL AND \"IdentificacaoRegistro\" != ''");
-            SelectCustomizado1($"SELECT COUNT(*) FROM \"ContratosFornecedores\" WHERE \"PrestacaoContaId\" IN ({txtPrestacoes}) AND \"Ativo\" = 'TRUE' AND \"IdentificacaoRegistro\" IS NULL");
 
-            Console.WriteLine(
+            for (int i = 0; i < txtPrestacoes.Length; i++)
+            {
+                qtdRemovida = 0;
+                flagIrregular = false;
+                deletadas = new List<int>();
+
+                Console.WriteLine(
                 string.Format(
-                    "Quantidade Sem IdentificacaoRegistro = ({0})",
-                    qtdParaAnalise
+                    "INICIO PrestacaoContaId = ({0})",
+                    txtPrestacoes[i]
                     )
                 );
+
+                Console.WriteLine();
+
+                SelectContaFinanceirasCustomizado1($"SELECT \"IdentificacaoRegistro\", \"Id\", \"ContaContabil\" FROM \"ContaFinanceiras\" WHERE \"PrestacaoContaId\" IN ({txtPrestacoes[i]}) ORDER BY \"Id\"");
+
+                Console.WriteLine(
+                string.Format(
+                    "FIM PrestacaoContaId = ({0})",
+                    txtPrestacoes[i]
+                    )
+                );
+
+                Console.WriteLine();
+
+                msgFinalQtdRemovida.Add(qtdRemovida.ToString());
+                msgFinalIrregular.Add(flagIrregular);
+                deletadasPorPrestacao.Add(deletadas);
+
+            }
+
+            for (int i = 0; i < txtPrestacoes.Length; i++)
+            {
+                Console.WriteLine(
+                string.Format(
+                    "PrestacaoContaId = ({0})",
+                    txtPrestacoes[i]
+                    )
+                );
+
+                Console.WriteLine(
+                string.Format(
+                    "Quantidade de Contas Financeiras (Duplicadas) Remanejados os DocFinanceiros e Removidas = ({0})",
+                    msgFinalQtdRemovida[i]
+                    )
+                );
+
+                for (int j = 0; j < deletadasPorPrestacao[i].Count; j++)
+                {
+                    Console.WriteLine(
+                    string.Format(
+                        "Conta Financeira (Duplicadas) Removida Id = ({0})",
+                        deletadasPorPrestacao[i][j]
+                        )
+                    );
+                }
+
+                if(msgFinalIrregular[i])
+                    Console.WriteLine("Há Contas Financeiras com o mesmo IdentificacaoRegistro e com ContaContabil diferente. Verificar com a OS qual das Contas não existe mais, remanegar os DocFinanceiros e deletar a mesma!");
+
+                Console.WriteLine();
+            }
+
+            //SelectCustomizado1($"SELECT COUNT(*) FROM \"ContratosFornecedores\" WHERE \"PrestacaoContaId\" IN ({txtPrestacoes}) AND \"IdentificacaoRegistro\" IS NOT NULL AND \"IdentificacaoRegistro\" != ''");
+            //SelectCustomizado2($"SELECT \"PrestacaoContaId\", \"NumeroContrato\", \"IdentificacaoRegistro\" FROM \"ContratosFornecedores\" WHERE \"PrestacaoContaId\" IN ({txtPrestacoes}) AND \"IdentificacaoRegistro\" IS NOT NULL AND \"IdentificacaoRegistro\" != ''");
+            //SelectCustomizado1($"SELECT COUNT(*) FROM \"ContratosFornecedores\" WHERE \"PrestacaoContaId\" IN ({txtPrestacoes}) AND \"Ativo\" = 'TRUE' AND \"IdentificacaoRegistro\" IS NULL");
 
             /*Insert();
             Select();
@@ -121,7 +186,7 @@ namespace Driver
             Console.ReadLine();
         }
 
-        static void SelectCustomizado1(string txtQuery)
+        static void SelectContaFinanceirasCustomizado1(string txtQuery)
         {
             // Build connection string using parameters from portal
             //
@@ -147,22 +212,226 @@ namespace Driver
                     var reader = command.ExecuteReader();
                     while (reader.Read())
                     {
+                        /*
                         Console.WriteLine(
                             string.Format(
                                 "Quantidade a analisar = ({0})",
-                                reader.GetInt32(0).ToString()
+                                reader.GetString(0).ToString()
                                 )
                             );
                         qtdParaAnalise = reader.GetInt32(0);
+                        */
+
+                        CorrecaoContaFinanceirasCustomizado1(txtQuery, reader.GetString(0), reader.GetInt32(1), reader.GetString(2));
                     }
                     reader.Close();
                 }
             }
 
-            Console.WriteLine("Press RETURN to exit");
-            Console.ReadLine();
+            //Console.WriteLine("Press RETURN to exit");
+            //Console.ReadLine();
         }
 
+        static void CorrecaoContaFinanceirasCustomizado1(string txtQuery, string identificacaoRegistro, int id, string contaContabil)
+        {
+            // Build connection string using parameters from portal
+            //
+            string connString =
+                String.Format(
+                    "Server={0}; User Id={1}; Database={2}; Port={3}; Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+
+                Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+
+                using (var command = new NpgsqlCommand(txtQuery, conn))
+                {
+                    bool flagDeletado;
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        if(reader.GetString(0) == identificacaoRegistro && reader.GetInt32(1) != id && reader.GetString(2) == contaContabil)
+                        {
+                            int qtdId1 = SelectDocFinanceirosCustomizado1($"SELECT \"Id\" FROM \"DocFinanceiros\" WHERE \"ContaFinanceiraId\" IN ({id})");
+                            int qtdId2 = SelectDocFinanceirosCustomizado1($"SELECT \"Id\" FROM \"DocFinanceiros\" WHERE \"ContaFinanceiraId\" IN ({reader.GetInt32(1)})");
+
+                            flagDeletado = false;
+
+                            if (qtdId1 >= qtdId2)
+                            {
+                                deletadas.ForEach((c) => { if (c == reader.GetInt32(1)) flagDeletado = true; });
+                                if (!flagDeletado)
+                                {
+                                    if (qtdId2 == 0)
+                                    {
+
+                                        DeleteContaFinanceirasCustomizado1(reader.GetInt32(1));
+                                        deletadas.Add(reader.GetInt32(1));
+
+                                    }
+                                    else
+                                    {
+                                        UpdateDocFinanceirosCustomizado1(id, reader.GetInt32(1));
+
+                                        DeleteContaFinanceirasCustomizado1(reader.GetInt32(1));
+                                        deletadas.Add(reader.GetInt32(1));
+
+                                    }
+                                }
+                            }
+                            else if (qtdId1 < qtdId2)
+                            {
+                                deletadas.ForEach((c) => { if (c == id) flagDeletado = true; });
+                                if (!flagDeletado)
+                                {
+                                    if (qtdId1 == 0)
+                                    {
+
+                                        DeleteContaFinanceirasCustomizado1(id);
+                                        deletadas.Add(id);
+                                        id = reader.GetInt32(1);
+
+                                    }
+                                    else
+                                    {
+                                        UpdateDocFinanceirosCustomizado1(reader.GetInt32(1), id);
+
+                                        DeleteContaFinanceirasCustomizado1(id);
+                                        deletadas.Add(id);
+                                        id = reader.GetInt32(1);
+
+                                    }
+                                }
+                            }
+                        }
+                        else if (reader.GetString(0) == identificacaoRegistro && reader.GetInt32(1) != id && reader.GetString(2) != contaContabil)
+                        {
+                            flagIrregular = true;
+                        }
+                    }
+                    reader.Close();
+                }
+            }
+
+            //Console.WriteLine("Press RETURN to exit");
+            //Console.ReadLine();
+        }
+
+        static void DeleteContaFinanceirasCustomizado1(int id)
+        {
+            // Build connection string using parameters from portal
+            //
+            string connString =
+                String.Format(
+                    "Server={0}; User Id={1}; Database={2}; Port={3}; Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("DELETE FROM \"ContaFinanceiras\" WHERE \"Id\" = @i", conn))
+                {
+                    command.Parameters.AddWithValue("i", id);
+
+                    int nRows = command.ExecuteNonQuery();
+                    Console.Out.WriteLine(String.Format("Number of rows deleted={0}", nRows));
+                    qtdRemovida++;
+                }
+            }
+
+            //Console.WriteLine("Press RETURN to exit");
+            //Console.ReadLine();
+        }
+
+        static int SelectDocFinanceirosCustomizado1(string txtQuery)
+        {
+            // Build connection string using parameters from portal
+            //
+            string connString =
+                String.Format(
+                    "Server={0}; User Id={1}; Database={2}; Port={3}; Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+
+                Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+
+                using (var command = new NpgsqlCommand(txtQuery, conn))
+                {
+
+                    var reader = command.ExecuteReader();
+
+                    int qtdDocFinanceiros = 0;
+                    while (reader.Read())
+                    {
+                        qtdDocFinanceiros++;
+                    }
+
+                    reader.Close();
+
+                    return qtdDocFinanceiros;
+                }
+            }
+
+            //Console.WriteLine("Press RETURN to exit");
+            //Console.ReadLine();
+        }
+        
+        static void UpdateDocFinanceirosCustomizado1(int idp, int id)
+        {
+            // Build connection string using parameters from portal
+            //
+            string connString =
+                String.Format(
+                    "Server={0}; User Id={1}; Database={2}; Port={3}; Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+
+                //Console.Out.WriteLine("Opening connection");
+                conn.Open();
+
+                using (var command = new NpgsqlCommand("UPDATE \"DocFinanceiros\" SET \"ContaFinanceiraId\" = @cfidp WHERE \"ContaFinanceiraId\" = @cfid", conn))
+                {
+                    command.Parameters.AddWithValue("cfidp", idp);
+                    command.Parameters.AddWithValue("cfid", id);
+                    int nRows = command.ExecuteNonQuery();
+                    Console.Out.WriteLine(String.Format("Number of rows updated={0}", nRows));
+                }
+            }
+
+            //Console.WriteLine("Press RETURN to exit");
+            //Console.ReadLine();
+        }
+        
+        /*
         static void SelectCustomizado2(string txtQuery)
         {
             // Build connection string using parameters from portal
@@ -267,6 +536,7 @@ namespace Driver
             //Console.WriteLine("Press RETURN to exit");
             //Console.ReadLine();
         }
+        */
 
         static void Delete()
         {
